97501c409543f9335635e20d9e61e08a83e032bb
zyf666 <3042760692@qq.com>
2023-04-01 21:31:43 +0800

    initial

---
+++ typing/global.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="react-scripts" />
+//声明了这些文件类型可以通过 import 语句导入，并且不需要写类型声明文件；
+declare module '*.png'
+declare module '*.gif'
+declare module '*.jpg'
+declare module '*.jfif'
+declare module '*.jpeg'
+declare module '*.svg'
+declare module '*.css'
+declare module '*.scss'
+declare module '*.less'
+declare module '*.sass'
+declare module '*.styl'
+declare module '@emoji-mart/react'
